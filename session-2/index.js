a23932ad49437e9ea928a2ee54fbd1c9db081557
Brenda <uchejiafubrenda@gmail.com>
2022-01-24 20:56:01 +0000

    added boolean

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,26 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+console.log ('Hello everyone!')
+console.log ('Hey,' + 'how are you?')
+
+console.log (document.querySelector('.about').innerHTML);
+
+console.log (document.querySelectorAll('.about'));
+
+
+
+
+const a = 5;
+const b = 10;
+let c = a + b;
+
+console.log ("Original output: "+ c);
+ 
+a = 20;
+
+console.log ("After 'a' has been updated" + c);
+
+const c = a + b;
+
+console.log ("After 'c' has been updated to use the new 'a' value" + c);
--- session-4/index.js
+++ session-4/index.js
@@ -1,5 +1,38 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+    name: 'Monica', 
+    age: '24', 
+    location: 'Paris',
+    likes: 'ice skating'
+};
 
+let personB = {
+    name: 'Valentina',
+    age: '18', 
+    location: 'Milan',
+    likes:'pizza'
+};
+
+let personC = {
+    name: 'Amara', 
+    age: '33',
+    location: 'London', 
+    likes: 'music'
+}; 
+
+let personD = {
+    name: 'Francine',
+    age: '21', 
+    location: 'Barceloona', 
+    likes: 'travelling'
+}; 
+ 
+function biography(person){ 
+console.log(person); 
+return "Hi, my name is " + person.name + " I am " + person.age + " I live in " + person.location + " I like " + person.likes; 
+
+}
+ 
 
 
 
@@ -19,25 +52,28 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+ const x = 6;
+ const y = 4;
 
-// const addition = x + y;
+ const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+ console.log(addition);
 
-// const subtraction = x - y;
+ const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+ console.log(subtraction)
 
-// const multiplication = x * y;
+ const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+ console.log(multiplication);
 
-// const division = x * y;
+ const division = x * y;
 
-// console.log("Division: x + y " + division);
+ console.log(division);
 
+ const mathProblem = x * y / x; 
+
+ console.log(mathProblem); 
 
 
 
@@ -54,8 +90,11 @@
 
 // ****************************************************TASK 3****************************************************
 
+const myAge= 22;
+const drivingMinimumAge= 18;
 
-
+const amIeligibleToDrive = (myAge > drivingMinimumAge);
+console.log(' Am I old enough to drive? ' + amIeligibleToDrive); 
 
 
 

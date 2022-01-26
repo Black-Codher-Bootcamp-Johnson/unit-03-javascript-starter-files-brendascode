2c52b6773cefdd7e120f1e17c7ff3347ebde5877
Brenda <uchejiafubrenda@gmail.com>
2022-01-26 20:06:26 +0000

    added switch statement

--- session-5/index.js
+++ session-5/index.js
@@ -18,7 +18,7 @@ function getGrade (total){
     console.log("E");
 
     else if (total > 30 && total  <= 40)
-    console.log ("F"); 
+    console.log("F"); 
 
     else (total  < 30)
     console.log("Fail"); 
@@ -26,3 +26,59 @@ function getGrade (total){
 }
 
 console.log(getGrade (40)); 
+
+// let grade 
+// if (total > 90){
+// grade= "A+"; 
+// }
+// console.log ("Your grade is" + grade) 
+
+
+//switch statements do not use comparison operators 
+//therefore the exercise completely changes
+//switch can replace multiple "if" and gives a more descriptive way 
+//to compare a value with multiple variants
+
+
+function getGrade(total){
+    let grade;
+  
+        switch (true) {
+        case (total > 90):
+        grade = 'A+';
+        break;
+  
+        case (total > 80):
+        grade = 'A';
+        break;
+  
+        case (total > 70):
+        grade = 'B';
+        break;
+  
+        case (total > 60):
+        grade = 'C';
+        break;
+  
+        case (total > 50):
+        grade = 'D';
+        break;
+  
+        case (total > 40):
+        grade = 'E';
+        break;
+  
+        case (total > 30):
+        grade = 'F';
+        break;
+  
+        case (total < 30):
+        grade = 'FAIL';
+        break;
+  
+        default:
+        grade = 'Not found';
+      }
+      return grade
+  }
+  console.log("Your grade is: " + getGrade(95)+"!");
